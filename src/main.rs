a0541ac2e7a8e8a9d1c8848bfdf3e1729a58775f
smoothsea <smoothsea>
2020-05-24 23:28:15 +0800

    add

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,12 @@ use std::env;
 use std::fs::{self, File};
 use std::path::{Path, PathBuf};
 use std::io::prelude::*;
+use std::io::{SeekFrom};
+use std::str;
 use std::error::Error;
+use serde::{Serialize, Deserialize};
+use chrono::offset::Utc;
+use chrono::DateTime;
 
 #[derive(Debug)]
 struct Args {
@@ -23,11 +28,12 @@ struct IndexElement {
     class: String,
     name: String,
     date: String,
+    size: u64,
 }
 
 impl IndexElement {
-    fn new(class: String, name: String, date: String) -> IndexElement {
-        IndexElement { class, name, date }
+    fn new(class: String, name: String, date: String, size: u64) -> IndexElement {
+        IndexElement { class, name, date, size }
     }
 }
 
@@ -35,12 +41,13 @@ impl IndexElement {
 struct IndexRender {
     status: bool,
     info: String,
-    list: Vec<IndexElement>,
+    list: String,
 }
 
 impl IndexRender {
     fn new(status: bool, info: String, list: Vec<IndexElement>) -> IndexRender {
-        IndexRender { status, info, list }
+        let list_json = serde_json::to_string(& list).expect("error");
+        IndexRender { status, info, list:list_json}
     }
 }
 
@@ -50,15 +57,19 @@ impl Args {
     }
 }
 
-#[derive(Debug, Serialize)]
+#[derive(Debug, Serialize, Deserialize)]
 struct DetailRender {
     content: String,    
+    seek: u64,
+    file_path: String,
 }
 
 impl DetailRender {
-    fn new(content: String) -> DetailRender {
+    fn new(content: String, file_path: String, seek: u64) -> DetailRender {
         DetailRender {
-            content
+            content,
+            seek,
+            file_path
         }
     }
 }
@@ -84,12 +95,18 @@ fn get_directory_info_render(dir: &str) -> Result<IndexRender, Box<dyn Error>> {
         let mut elements: Vec<IndexElement> = vec![];
 
         for entry in fs::read_dir(path)? {
-            let file_path = entry?.path();
+            let entry = entry?;
+            let file_path = entry.path();
+            let metadata = entry.metadata()?;
             let mut file_name = file_path
                 .file_name()
                 .unwrap()
                 .to_string_lossy()
                 .into_owned();
+
+            let len = metadata.len();
+            let atime: DateTime<Utc> = metadata.modified()?.into();
+            let atime_string = atime.format("%Y-%m-%d %T").to_string();
 
             let mut class = "".to_string();
             if (file_path.is_dir()) {
@@ -98,7 +115,7 @@ fn get_directory_info_render(dir: &str) -> Result<IndexRender, Box<dyn Error>> {
                 class = "f".to_string();
             }
             
-            elements.push(IndexElement::new(class, file_name, "x".to_string()));
+            elements.push(IndexElement::new(class, file_name, atime_string, len));
         }
         render = IndexRender::new(true, "".to_string(), elements);
     } else {
@@ -108,14 +125,53 @@ fn get_directory_info_render(dir: &str) -> Result<IndexRender, Box<dyn Error>> {
     Ok(render)
 }
 
-fn get_detail_render(file_path: &str) -> Result<DetailRender, Box<dyn Error>> {
+fn get_detail_render(file_path: &str, start_seek: u64) -> Result<DetailRender, Box<dyn Error>> {
     let path = Path::new(file_path);
     let mut file = File::open(path)?;
+    let metadata = file.metadata()?;
+    let file_len = metadata.len();
+    let max_file_len = 5181440;
+    
     let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
+    let mut seek = file_len;
+    if (file_len > max_file_len || start_seek > 0) {
+        let len_if_exceed = 512000; //500kb
+        let mut read_start_seek:u64;
+        if (start_seek > 0) {
+            read_start_seek = start_seek;
+        } else {
+            read_start_seek = file_len - len_if_exceed;
+        }
+        if let (a, b) = attemp_to_read_file(&mut file, read_start_seek, 3)? {
+            contents = a;
+        }
+    } else {
+        file.read_to_string(&mut contents)?;
+    }
 
-    let render = DetailRender::new(contents);
+    let render = DetailRender::new(contents, file_path.to_string(), seek);
     Ok(render)
+}
+
+fn attemp_to_read_file(file:&mut File, seek:u64, times:u8) -> Result<(String, u64), Box<dyn Error>> {
+    let mut content = "".to_string();
+    let mut buff = vec![];
+    file.seek(SeekFrom::Start(seek));
+    file.read_to_end(&mut buff);
+    match String::from_utf8(buff) {
+        Ok(s) => {
+            return Ok((s, seek));
+        },
+        Err(e) => {
+            if (times > 0) {
+                return attemp_to_read_file(file, seek + 1, times - 1)
+            } else {
+                return Err(Box::new(e));
+            }
+        }
+    }
+
+    Ok((content, seek))
 }
 
 
@@ -132,7 +188,25 @@ fn index(args: State<Args>) -> Template {
     };
 }
 
-#[get("/<name..>")]
+
+#[get("/more?<seek>&<path>", rank = 3)]
+fn more(args: State<Args>, seek:u64, path:String) -> String {
+    let mut output = "".to_string();
+    match get_detail_render(&path, seek) {
+        Ok(render) => {
+           if let Ok(a) =  serde_json::to_string(& render) {
+               output = a;
+           }
+        },
+        Err(e) => {
+            output = e.to_string();
+        }
+    }
+    output
+}
+
+
+#[get("/<name..>", rank = 4)]
 fn detail(args: State<Args>, name: PathBuf) -> Template {
     let path = &args.file_dir;
     let full_file_name = format!("{}{}", path, name.to_string_lossy().into_owned());
@@ -148,7 +222,7 @@ fn detail(args: State<Args>, name: PathBuf) -> Template {
             } 
         };
     } else {
-        match get_detail_render(&full_file_name) {
+        match get_detail_render(&full_file_name, 0) {
             Ok(render) => {
                 return Template::render("detail", render);
             },
@@ -164,7 +238,7 @@ fn main() {
     let args: Args = parse_arguments();
     let app = rocket::ignite()
         .manage(args)
-        .mount("/", routes![index, detail])
+        .mount("/", routes![index, detail, more])
         .attach(Template::fairing());
     app.launch();
 }
@@ -176,10 +250,10 @@ fn parse_arguments() -> Args {
         panic!("Arguments can't be empty");
     }
     let mut file_dir: String = (&args[1]).to_string();
-    file_dir.pop();
     if let Some(o) = file_dir.pop() {
         if (o.to_string() != "/".to_string()) {
-            file_dir = format!("{}{}", file_dir, o);
+            println!("ok");
+            file_dir = format!("{}{}", file_dir, o.to_string());
         }
         file_dir = format!("{}{}", file_dir, "/".to_string());
     };
